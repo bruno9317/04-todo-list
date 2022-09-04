d886bee2f6ab25ed4691ce4435b69a07a2f48281
bruno <brunobaptista9317@gmail.com>
2022-09-04 11:30:55 -0300

    salvando boas praticas

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-let elementoOl = document.createElement("ol");
+const elementoOl = document.createElement('ol');
 elementoOl.id = 'lista-tarefas';
 document.body.appendChild(elementoOl);
 
-let botao = document.getElementById('criar-tarefa');
-botao.addEventListener('click', recebeClick)
-
 function recebeClick() {
-    let tarefaNoInput = document.getElementById('texto-tarefa').value;
-    let lista = document.getElementById('lista-tarefas');
-    let elementoIl = document.createElement('li');
-    elementoIl.innerText = tarefaNoInput;
-    lista.appendChild(elementoIl);
-
-    document.getElementById('texto-tarefa').value = '';
+  const tarefaNoInput = document.getElementById('texto-tarefa').value;
+  const lista = document.getElementById('lista-tarefas');
+  const elementoIl = document.createElement('li');
+  elementoIl.innerText = tarefaNoInput;
+  lista.appendChild(elementoIl);
+  document.getElementById('texto-tarefa').value = '';
 }
+
+const botao = document.getElementById('criar-tarefa');
+botao.addEventListener('click', recebeClick);
