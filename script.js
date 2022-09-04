86a529694740a21f55a7d9ff0192aefd02f5e5e3
bruno <brunobaptista9317@gmail.com>
2022-09-04 11:19:53 -0300

    salvando requisito 5 e 6

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 let elementoOl = document.createElement("ol");
 elementoOl.id = 'lista-tarefas';
 document.body.appendChild(elementoOl);
+
+let botao = document.getElementById('criar-tarefa');
+botao.addEventListener('click', recebeClick)
+
+function recebeClick() {
+    let tarefaNoInput = document.getElementById('texto-tarefa').value;
+    let lista = document.getElementById('lista-tarefas');
+    let elementoIl = document.createElement('li');
+    elementoIl.innerText = tarefaNoInput;
+    lista.appendChild(elementoIl);
+
+    document.getElementById('texto-tarefa').value = '';
+}
