084bb12076f7da8a2b5507516eccc07d9959b048
bruno <brunobaptista9317@gmail.com>
2022-09-05 11:19:57 -0300

    salvando boas praticas

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const elementoOl = document.createElement('ol');
 elementoOl.id = 'lista-tarefas';
 document.body.appendChild(elementoOl);
 
+function recebeClick2(e) {
+  const listaDeLi = document.getElementsByClassName('selected');
+  for (let index = 0; index < listaDeLi.length; index += 1) {
+    document.getElementsByClassName('selected')[index].classList.remove('selected');
+  }
+  e.target.classList.add('selected');
+}
+
 function recebeClick() {
   const tarefaNoInput = document.getElementById('texto-tarefa').value;
   const lista = document.getElementById('lista-tarefas');
@@ -14,13 +22,3 @@ function recebeClick() {
 
 const botao = document.getElementById('criar-tarefa');
 botao.addEventListener('click', recebeClick);
-
-function recebeClick2(e){
-  let listaDeLi = document.getElementsByClassName('selected');
-  for(let index = 0; index < listaDeLi.length; index += 1){
-    console.log('entrou');
-    document.getElementsByClassName('selected')[index].classList.remove('selected');
-  }
-  
-  e.target.classList.add("selected");
-}
