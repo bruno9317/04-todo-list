a248f07b845809f50d4579ac4e8d2a0e8e5b8bf1
bruno <brunobaptista9317@gmail.com>
2022-09-05 13:45:31 -0300

    salvando requisito 9

--- script.js
+++ script.js
@@ -10,12 +10,28 @@ function recebeClick2(e) {
   e.target.classList.add('selected');
 }
 
+function recebeClick3(e){
+  const listaDeLi = e.target.classList;
+  let sera = false;
+  for (let index = 0; index < listaDeLi.length; index += 1) {
+    if(listaDeLi[index] === 'completed'){
+      sera = true;
+    }
+  }
+  if(sera === true){
+    e.target.classList.remove('completed');
+  }else{
+    e.target.classList.add('completed');
+  }
+}
+
 function recebeClick() {
   const tarefaNoInput = document.getElementById('texto-tarefa').value;
   const lista = document.getElementById('lista-tarefas');
   const elementoIl = document.createElement('li');
   elementoIl.innerText = tarefaNoInput;
   elementoIl.addEventListener('click', recebeClick2);
+  elementoIl.addEventListener('dblclick', recebeClick3);
   lista.appendChild(elementoIl);
   document.getElementById('texto-tarefa').value = '';
 }
