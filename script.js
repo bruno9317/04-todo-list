702cb87e1c4d8913da54db93503a49cba604589d
bruno <brunobaptista9317@gmail.com>
2022-09-05 10:50:42 -0300

    salvando requisito 7

--- script.js
+++ script.js
@@ -7,9 +7,24 @@ function recebeClick() {
   const lista = document.getElementById('lista-tarefas');
   const elementoIl = document.createElement('li');
   elementoIl.innerText = tarefaNoInput;
+  elementoIl.addEventListener('click', recebeClick2);
   lista.appendChild(elementoIl);
   document.getElementById('texto-tarefa').value = '';
 }
 
 const botao = document.getElementById('criar-tarefa');
 botao.addEventListener('click', recebeClick);
+
+function recebeClick2(e){
+ e.target.style.backgroundColor = 'grey';
+
+}
+// function criaBotao(){
+//     let listaDeItens = document.getElementsByTagName('li');
+//     let botoesLi = [];
+//     for(let index = 0; index < listaDeItens.length; index += 1){
+//         botoesLi[index] = listaDeItens[index];
+//         botoesLi[index].addEventListener('click', recebeClick2);
+//     }
+// }
+// criaBotao();
